8e8ea1b5b0cf2b448a590920421a63a742a5862b
Sarah L. Rimron-Soutter <sarah.soutter@gmail.com>
2015-10-13 14:22:12 +0100

    tinkering with oop

--- fcc-pomodoro-timer/js/index.js
+++ fcc-pomodoro-timer/js/index.js
@@ -1,18 +1,32 @@
-var minutes = 10;
+var Session = function(length){
+    this.length=length;
+};
+
+
+Session.prototype.increaseLength = function(amount, update){
+    this.length += amount;
+    update.html(this.length);
+};
+
+Session.prototype.decreaseLength = function(amount, update){
+    if((this.length - amount) > 0){
+        this.length = this.length - amount;
+        console.log(this.length);
+        update.html(this.length);
+    }
+};
+
+var workSession = new Session(25);
+var breakSession = new Session(5);
+
+
+
+var minutes = workSession.length;
 var seconds = '00';
 var counter= minutes * 60;
 var timer = {
     state:'stopped'
 };
-
-function increment(){
-    counter++;
-    seconds = counter%60;
-    if (seconds === 0){
-        minutes++;
-    }
-    $(".main-timer").html(minutes + ':' + seconds);
-}
 
 function decrement(){
 
@@ -35,16 +49,36 @@ function startStop(timer) {
     } else {
         clearInterval(timer.ID);
         timer.state = 'stopped';
+
     }
 }
 
 
-
 $(document).ready(function() {
+
+
         $(".main-timer").html(minutes + ':' + seconds);
+        $(".current-break").html(5);
+        $(".current-session").html(workSession.length);
 
         $(".main-timer").click(function () {
             startStop(timer);
+        });
+
+        $(".change-break > .add").click(function(){
+            breakSession.increaseLength(1, $(this).siblings().last());
+        });
+
+        $(".change-timer >.add").click(function(){
+            workSession.increaseLength(5, $(this).siblings().last());
+        });
+
+        $(".change-break > .minus").click(function(){
+            breakSession.decreaseLength(1, $(this).siblings().last());
+        });
+
+        $(".change-timer > .minus").click(function(){
+            workSession.decreaseLength(5, $(this).siblings().last());
         });
     }
     );
